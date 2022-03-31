d999ed728e923667aae076b20353710280a4834e
Alex <alexbachurin91@gmail.com>
2022-03-31 07:05:44 +0300

    AuthContext setup and functionality

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,77 @@
+import React, { useState, useContext, useCallback, useEffect, useMemo } from "react";
+import Cookies from "js-cookie";
+import api from '../services/index'
+const AuthContext = React.createContext();
+
+
+const AuthProvider = ({ children }) => {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [user, setUser] = useState(null);
+    const [token, setTokenData] = useState(null);
+
+    //setToken
+    const setToken = useCallback((tokenData) => {
+        setTokenData(tokenData);
+
+        if (tokenData) {
+            Cookies.set("auth-token", tokenData);
+        } else {
+            Cookies.remove("auth-token");
+        }
+    }, []);
+    //LOGOUT
+    const logOut = useCallback(() => {
+        setUser(null);
+        setToken(null);
+    }, [setToken]);
+
+    //GET USER INFO
+    const loadData = useCallback(async () => {
+        const tokenData = Cookies.get("auth-token");
+        setTokenData(tokenData);
+
+        try {
+            if (tokenData) {
+                const { data } = await api.auth.getProfile();
+                setUser(data);
+            }
+        } catch {
+            setToken(null);
+        } finally {
+            setIsLoaded(true);
+        }
+    }, [setToken]);
+
+    useEffect(() => {
+        loadData();
+    }, [loadData]);
+
+
+    //ВСЕ ВАЛЬЮСЫ что передаем из контекста кэшируем и передаем в контекст
+    const contextValue = useMemo(
+        () => ({
+            isLoaded,
+            user,
+            token,
+            setUser,
+            setToken,
+            logOut,
+        }),
+        [isLoaded, user, token, setToken, logOut]
+    );
+
+
+
+
+    return <AuthContext.Provider value={{
+        contextValue
+    }}>
+        {children}
+    </AuthContext.Provider>
+}
+//global hook
+const useAuthContext = () => {
+    return useContext(AuthContext);
+}
+
+export { AuthProvider, useAuthContext }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { AppProvider } from './contexts/AppContext'
-
+import { AuthProvider } from './contexts/AuthContext'
 ReactDOM.render(
   <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    <AuthProvider>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
